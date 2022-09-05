3f4567d42979ac1482a6a205a5ba3e17b58cd661
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2022-09-05 15:34:40 -0300

    Fixing interface generic type

--- src/Services/Catalog/Catalog.DataAccess/CatalogContextSeed.cs
+++ src/Services/Catalog/Catalog.DataAccess/CatalogContextSeed.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using Catalog.API;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Catalog.DataAccess {
-	public class CatalogContextSeed {
+	public class CatalogContextSeed : ICatalogContextSeed {
+		public Task SeedAsync(CatalogContext catalogContext, IWebHostEnvironment webHostEnvironment,IOptions<CatalogOptions> settings, ILogger<CatalogContextSeed> logger) {
+			throw new NotImplementedException();
+		}
 	}
 }
--- src/Services/Catalog/Catalog.DataAccess/Repositories/ICatalogBrandRepository.cs
+++ src/Services/Catalog/Catalog.DataAccess/Repositories/ICatalogBrandRepository.cs
@@ -1,6 +1,6 @@
 ﻿using Catalog.Core.Models;
 
 namespace Catalog.DataAccess.Repositories {
-	public interface ICatalogBrandRepository : IRepository<CatalogType> {
+	public interface ICatalogBrandRepository : IRepository<CatalogBrand> {
 	}
 }
