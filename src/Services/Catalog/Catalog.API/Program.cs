eb9f90c061123241699b7113552a3290a4047931
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2022-09-05 15:09:21 -0300

    Adding CatalogOptions, CatalogContext, & CatalogContextSeed

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,40 @@
+using Catalog.API;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebAPI {
+	public class Program {
+		public static void Main(string[] args) {
+			WebApplicationBuilder webApplicationBuilder = WebApplication.CreateBuilder(args);
+			AddServicesToContainer(webApplicationBuilder);
+			WebApplication webApplication = webApplicationBuilder.Build();
+			ConfigureHttpRequestPipeline(webApplication);
+			webApplication.Run();
+		}
+
+		private static void AddServicesToContainer(WebApplicationBuilder webApplicationBuilder) {
+			webApplicationBuilder.Services.AddControllers();
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+			webApplicationBuilder.Services.AddEndpointsApiExplorer();
+			webApplicationBuilder.Services.AddSwaggerGen();
+			webApplicationBuilder.Services.Configure<CatalogOptions>(
+				webApplicationBuilder.Configuration.GetSection(CatalogOptions.KEY));
+		}
+
+		private static void ConfigureHttpRequestPipeline(WebApplication webApplication) {
+			if (webApplication.Environment.IsDevelopment()) {
+				webApplication.UseSwagger();
+				webApplication.UseSwaggerUI(options => {
+					options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+					options.RoutePrefix = string.Empty;
+				});
+			}
+
+			webApplication.UseHttpsRedirection();
+			webApplication.UseAuthorization();
+			webApplication.MapControllers();
+		}
+	}
+}
---
+++ src/Services/Catalog/Catalog.DataAccess/ICatalogContextSeed.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Hosting;
+using Catalog.API;
+
+namespace Catalog.DataAccess {
+	public interface ICatalogContextSeed {
+		Task SeedAsync(CatalogContext catalogContext, IWebHostEnvironment webHostEnvironment, IOptions<CatalogOptions> settings, ILogger<CatalogContextSeed> logger);
+	}
+}
