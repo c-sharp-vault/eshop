cc3eabf058fa1a57de7a418234049ab7a6bdf52e
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2022-09-05 16:17:03 -0300

    Add Catalog.Infrastructure project & relocating CatalogOptions there

---
+++ src/Services/Catalog/Catalog.Infrastructure/Options/CatalogOptions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Catalog.Infrastructure.Options
+{
+    public class CatalogOptions
+    {
+
+        public const string KEY = "Catalog";
+
+        private string _picBaseUrl;
+        private string _eventBusConnection;
+        private bool _useCustomizationData;
+        private bool _azureStorageEnabled;
+
+        public string PicBaseUrl
+        {
+            get { return _picBaseUrl; }
+            set { _picBaseUrl = value; }
+        }
+
+        public string EventBusConnection
+        {
+            get { return _eventBusConnection; }
+            set { _eventBusConnection = value; }
+        }
+
+        public bool UseCustomizationData
+        {
+            get { return _useCustomizationData; }
+            set { _useCustomizationData = value; }
+        }
+
+        public bool AzureStorageEnabled
+        {
+            get { return _azureStorageEnabled; }
+            set { _azureStorageEnabled = value; }
+        }
+    }
+}
