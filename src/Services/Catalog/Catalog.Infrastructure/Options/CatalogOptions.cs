74fdd73ab04e6423eee19c6faf2fdf961887dc80
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2022-09-14 09:41:04 -0300

    Formatting CatalogOptions

--- src/Services/Catalog/Catalog.Infrastructure/Options/CatalogOptions.cs
+++ src/Services/Catalog/Catalog.Infrastructure/Options/CatalogOptions.cs
@@ -1,39 +1,29 @@
-﻿using System;
+﻿namespace Catalog.Infrastructure.Options {
+	public class CatalogOptions {
 
-namespace Catalog.Infrastructure.Options
-{
-    public class CatalogOptions
-    {
+		private string _picBaseUrl;
+		private string _eventBusConnection;
+		private bool _useCustomizationData;
+		private bool _azureStorageEnabled;
 
-        public const string KEY = "Catalog";
+		public string PicBaseUrl {
+			get { return _picBaseUrl; }
+			set { _picBaseUrl = value; }
+		}
 
-        private string _picBaseUrl;
-        private string _eventBusConnection;
-        private bool _useCustomizationData;
-        private bool _azureStorageEnabled;
+		public string EventBusConnection {
+			get { return _eventBusConnection; }
+			set { _eventBusConnection = value; }
+		}
 
-        public string PicBaseUrl
-        {
-            get { return _picBaseUrl; }
-            set { _picBaseUrl = value; }
-        }
+		public bool UseCustomizationData {
+			get { return _useCustomizationData; }
+			set { _useCustomizationData = value; }
+		}
 
-        public string EventBusConnection
-        {
-            get { return _eventBusConnection; }
-            set { _eventBusConnection = value; }
-        }
-
-        public bool UseCustomizationData
-        {
-            get { return _useCustomizationData; }
-            set { _useCustomizationData = value; }
-        }
-
-        public bool AzureStorageEnabled
-        {
-            get { return _azureStorageEnabled; }
-            set { _azureStorageEnabled = value; }
-        }
-    }
+		public bool AzureStorageEnabled {
+			get { return _azureStorageEnabled; }
+			set { _azureStorageEnabled = value; }
+		}
+	}
 }
