a1196a508a795f33bc4b9d503a9ffe3bd798fd5b
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2022-09-14 09:40:25 -0300

    Adding LINQ extension methods

---
+++ src/Services/Catalog/Catalog.Infrastructure/Extensions/Linq/SelectTryResult.cs
@@ -0,0 +1,28 @@
+﻿namespace Catalog.Infrastructure.Extensions.Linq {
+	public class SelectTryResult<TSource, TResult> {
+		private TSource _source;
+		private TResult _result;
+		private Exception _exception;
+
+		internal SelectTryResult(TSource source, TResult result, Exception exception) {
+			_source = source;
+			_result = result;
+			_exception = exception;
+		}
+
+		public TSource Source {
+			get { return _source; }
+			private set { _source = value; }
+		}
+
+		public TResult Result {
+			get { return _result; }
+			private set { _result = value; }
+		}
+
+		public Exception CaughtException {
+			get { return _exception; }
+			private set { _exception = value; }
+		}
+	}
+}
